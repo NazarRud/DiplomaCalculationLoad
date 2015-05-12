8fd71548861a376dff5221cb11dd625a4fb2c536
NazarRud <NazarRud@gmail.com>
2015-05-12 13:40:27 +0300

    Add TestClient Project for testing and update DB

---
+++ TestClient/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Linq;
+using Data;
+
+namespace TestClient
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (DataModel dm = new DataModel())
+            {
+                Console.WriteLine(dm.Database.Connection.ConnectionString);
+
+               Console.WriteLine(dm.Faculties.Count());
+                var a = dm.Cathedras.ToList();
+
+                foreach (var cathedra in a)
+                {
+                    Console.WriteLine(cathedra.Faculty.Name);
+                }
+            }
+        }
+    }
+}
