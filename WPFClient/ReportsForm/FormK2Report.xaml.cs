cdd866802acd3e02d89f012e826d9fa04eaf263c
Vladyslav Khrapov <vlad.hrapov@gmail.com>
2015-05-12 19:01:50 +0300

    Add reports forms

---
+++ WPFClient/ReportsForm/FormK2Report.xaml.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace WPFClient.ReportsForm
+{
+    /// <summary>
+    /// Логика взаимодействия для FormK2Report.xaml
+    /// </summary>
+    public partial class FormK2Report : Window
+    {
+        public FormK2Report()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ WPFClient/ReportsForm/FormK4Report.xaml.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace WPFClient.ReportsForm
+{
+    /// <summary>
+    /// Логика взаимодействия для FormK4Report.xaml
+    /// </summary>
+    public partial class FormK4Report : Window
+    {
+        public FormK4Report()
+        {
+            InitializeComponent();
+        }
+    }
+}
