4bec5ab3a4f68499528190fb548b92610a5c9db4
Vladyslav Khrapov <vlad.hrapov@gmail.com>
2015-05-18 04:05:07 +0300

    Update reports form

--- WPFClient/ReportsForm/FormReportsAll.xaml.cs
+++ WPFClient/ReportsForm/FormReportsAll.xaml.cs
@@ -12,6 +12,7 @@ using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
 
 using System.Text.RegularExpressions;
+using Reports.Public;
 
 namespace WPFClient.ReportsForm
 {
@@ -20,20 +21,43 @@ namespace WPFClient.ReportsForm
     /// </summary>
     public partial class FormReportsAll : Window
     {
+        private ReportsCreator reportsCreator;
         List<string> teachList = new List<string>();
 
         public FormReportsAll()
         {
             InitializeComponent();
+            reportsCreator = new ReportsCreator();
             InitCombo();
+            InitCombobox();
             //this.TeacherCombobox.SelectionChanged += new SelectionChangedEventHandler(TeacherCombobox_SelectionChanged);
-        }     
-
-        private void TeacherCombobox_SelectionChanged(object sender, SelectionChangedEventArgs e)
-        {
-            string s = TeacherCombobox.Text;
-            //MessageBox.Show("changed" + s);
         }
+
+        private void InitCombobox()
+        {
+            EduTypeK4Combobox.Items.Add("Бюджет");
+            EduTypeK4Combobox.Items.Add("Контракт");
+            EduTypeK4Combobox.Items.Add("Всі");
+            EduTypeK4Combobox.SelectedItem = EduTypeK4Combobox.Items[2];
+
+            EduTypeK5Combobox.Items.Add("Бюджет");
+            EduTypeK5Combobox.Items.Add("Контракт");
+            EduTypeK5Combobox.SelectedItem = EduTypeK5Combobox.Items[0];
+
+            TermOtherCombobox.Items.Add("I семестр");
+            TermOtherCombobox.Items.Add("II семестр");
+            TermOtherCombobox.Items.Add("Всі");
+            TermOtherCombobox.SelectedItem = TermOtherCombobox.Items[0];
+
+            DisciplineCombobox.Items.Add("Лекції");
+            DisciplineCombobox.Items.Add("Практичні заняття");
+            DisciplineCombobox.Items.Add("Лабораторні заняття");
+            DisciplineCombobox.Items.Add("Екзамени");
+            DisciplineCombobox.Items.Add("Розрахункові роботи");
+            DisciplineCombobox.Items.Add("Модульні роботи");
+            DisciplineCombobox.SelectedItem = DisciplineCombobox.Items[0];
+
+        }     
 
         private void InitCombo()
         {
@@ -60,6 +84,91 @@ namespace WPFClient.ReportsForm
         private void CloseButton_Click(object sender, RoutedEventArgs e)
         {
             this.Close();
+        }
+
+        private void CreateK2Button_Click(object sender, RoutedEventArgs e)
+        {
+            if (K2.IsChecked == true)
+            {
+                reportsCreator.CreateK2Async();
+            }
+            else if (TotalResults.IsChecked == true)
+            {
+                reportsCreator.CreatePlanAllTotalAsync();
+            }
+            else if (PlanAll.IsChecked == true)
+            {
+                reportsCreator.CreatePlanAllAsync();
+            }
+            else if (PaymentSum.IsChecked == true)
+            {
+                reportsCreator.CreatePaymentSumAsync();
+            }
+           
+        }
+
+        private void CreateK4Button_Click(object sender, RoutedEventArgs e)
+        {
+            if (EduTypeK4Combobox.SelectedItem == EduTypeK4Combobox.Items[0])
+            {
+                reportsCreator.CreateK4Async(EduType.Budget);
+            }
+            else if (EduTypeK4Combobox.SelectedItem == EduTypeK4Combobox.Items[1])
+            {
+                reportsCreator.CreateK4Async(EduType.Contract);
+            }
+            else
+            {
+                reportsCreator.CreateK4Async(EduType.Budget);
+                reportsCreator.CreateK4Async(EduType.Contract);
+            }
+            
+        }
+
+        private void CreateK5Button_Click(object sender, RoutedEventArgs e)
+        {
+            if (EduTypeK5Combobox.SelectedItem == EduTypeK5Combobox.Items[0])
+            {
+                reportsCreator.CreateK5Async(EduType.Budget);
+            }
+            else if (EduTypeK5Combobox.SelectedItem == EduTypeK5Combobox.Items[1])
+            {
+                reportsCreator.CreateK5Async(EduType.Contract);
+            }
+            else
+            {
+                reportsCreator.CreateK5Async(EduType.Budget);
+                reportsCreator.CreateK5Async(EduType.Contract);
+            }
+        }
+
+        private void CreateOtherButton_Click(object sender, RoutedEventArgs e)
+        {
+            reportsCreator.CreateLoadDistributionAsync(DisciplineCombobox.SelectedValue.ToString());
+        }
+
+        private void CreateDisciplineButton_Click(object sender, RoutedEventArgs e)
+        {
+            reportsCreator.CreatePlanAllDisciplineAsync();
+        }
+
+        private void EduTypeK5Combobox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            if (EduTypeK5Combobox.SelectedItem == EduTypeK5Combobox.Items[0])
+            {
+                TermK5Combobox.Items.Clear();
+                TermK5Combobox.Items.Add("I семестр");
+                TermK5Combobox.Items.Add("II семестр");
+                TermK5Combobox.Items.Add("Всі");
+                TermK5Combobox.SelectedItem = EduTypeK5Combobox.Items[0];
+            }
+            else if (EduTypeK5Combobox.SelectedItem == EduTypeK5Combobox.Items[1])
+            {
+                TermK5Combobox.Items.Clear();
+                TermK5Combobox.Items.Add("I семестр");
+                TermK5Combobox.Items.Add("II семестр");
+                TermK5Combobox.SelectedItem = EduTypeK5Combobox.Items[0];
+            }
         }   
 
     }
