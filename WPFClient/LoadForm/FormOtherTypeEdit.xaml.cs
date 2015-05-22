8a08a027f5644014fc8b24dd99caceb5967d44d9
NazarRud <NazarRud@gmail.com>
2015-05-23 02:08:35 +0300

    OtherType

--- WPFClient/LoadForm/FormOtherTypeEdit.xaml.cs
+++ WPFClient/LoadForm/FormOtherTypeEdit.xaml.cs
@@ -1,5 +1,9 @@
-﻿using System.Windows;
+﻿using System;
+using System.Linq;
+using System.Windows;
 using Data.Entity;
+using Data.Entity.Enum;
+using Data.Repository;
 
 namespace WPFClient.LoadForm
 {
@@ -8,10 +12,12 @@ namespace WPFClient.LoadForm
     /// </summary>
     public partial class FormOtherTypeEdit : Window
     {
+        private IUow _uow;
         public OtherType OtherType { get; set; }
-
+        private readonly App _app = Application.Current as App;
         public FormOtherTypeEdit()
         {
+            if (_app != null) _uow = _app.Uow;
             InitializeComponent();
         }
 
@@ -22,6 +28,25 @@ namespace WPFClient.LoadForm
 
         private void FormOtherTypeEdit_OnLoaded(object sender, RoutedEventArgs e)
         {
+            TypeWorkComboBox.ItemsSource = Enum.GetValues(typeof (TypeWork)).Cast<TypeWork>();
+            SubTypeWorkComboBox.ItemsSource = Enum.GetValues(typeof (SubTypeWork)).Cast<SubTypeWork>();
+            SemestrComboBox.ItemsSource = Enum.GetValues(typeof (Semestr)).Cast<Semestr>();
+            DataGridFlow.ItemsSource = _uow.Flow.All.ToList();
+        }
+
+        private void SaveButton_OnClick(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void CancleButton_OnClick(object sender, RoutedEventArgs e)
+        {
+            DialogResult = false;
+        }
+
+        private void CalculationButton_OnClick(object sender, RoutedEventArgs e)
+        {
+
         }
     }
 }
