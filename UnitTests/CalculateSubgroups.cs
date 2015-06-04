82297685e49d976116f933c83e51d321cfe0aacc
Vladyslav Khrapov <vlad.hrapov@gmail.com>
2015-06-05 01:12:22 +0300

    Add unit tests project

---
+++ UnitTests/CalculateSubgroups.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using NUnit.Framework;
+
+namespace UnitTests
+{
+    [TestFixture]
+    public class CalculateSubgroups
+    {
+
+        public int GetBudgetSubGroupsCount(int countStudentBudget)
+        {
+            int countSubGroupBudget;
+
+            if (countStudentBudget % 15 >= 10)
+                countSubGroupBudget = (countStudentBudget / 15) + 1;
+            else
+            {
+                countSubGroupBudget = (countStudentBudget / 15) < 1 ? 1 : (countStudentBudget / 15);
+                if (countStudentBudget / countSubGroupBudget >= 19)
+                    countSubGroupBudget++;
+            }
+
+            return countSubGroupBudget;
+        }
+
+        [Test]
+        public void BudgetSubGroupsCountTest()
+        {
+            Assert.AreEqual(3, GetBudgetSubGroupsCount(54));
+            Assert.AreEqual(11, GetBudgetSubGroupsCount(167));
+            Assert.AreEqual(2, GetBudgetSubGroupsCount(24));
+            Assert.AreEqual(1, GetBudgetSubGroupsCount(5));
+        }
+
+
+        public int GetContractSubGroupsCount(int countStudentContract)
+        {
+            int countSubGroupContract;
+
+            if (countStudentContract % 15 >= 10)
+                countSubGroupContract = (countStudentContract / 15) + 1;
+            else
+            {
+                countSubGroupContract = (countStudentContract / 15) < 1 ? 1 : (countStudentContract / 15);
+
+                if (countStudentContract / countSubGroupContract >= 19)
+                    countSubGroupContract++;
+            }
+
+            return countSubGroupContract;
+        }
+
+        [Test]
+        public void ContractSubGroupsCountTest()
+        {
+            Assert.AreEqual(5, GetContractSubGroupsCount(73));
+            Assert.AreEqual(2, GetContractSubGroupsCount(19));
+            Assert.AreEqual(2, GetContractSubGroupsCount(26));
+            Assert.AreEqual(1, GetContractSubGroupsCount(5));
+        }
+
+    }
+
+}
---
+++ UnitTests/Consultation.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using NUnit.Framework;
+
+namespace UnitTests
+{
+    [TestFixture]
+    public class Consultation
+    {
+        private int exam, tempAcademK, amountHours, tempStudCountK;
+        public double GetConsultationForDaily()
+        {
+            return 2 * exam * tempAcademK + 0.06 * amountHours * (tempStudCountK) / 25;
+        }
+
+        public double GetConsultationForAbsentee()
+        {
+            return 2 * exam * tempAcademK + 0.12 * amountHours * (tempStudCountK) / 25;
+        }
+
+        [SetUp]
+        public void SetUp()
+        {
+            exam = 30;
+            tempAcademK = 80;
+            amountHours = 67;
+            tempStudCountK = 34;
+        }
+
+        [Test]
+        public void CheckDailyConsultationTest()
+        {
+            Assert.That(4805.47, Is.EqualTo(Math.Round(GetConsultationForDaily(), 2)));
+        }
+
+        [Test]
+        public void CheckAbsenteeConsultationTest()
+        {
+            Assert.That(4810.93, Is.EqualTo(Math.Round(GetConsultationForAbsentee(), 2)));
+        }
+    }
+}
