727a3b3e6c29b7ad757e2acf9f2bdb8d5e66067d
Vladyslav Khrapov <vlad.hrapov@gmail.com>
2015-06-08 00:03:35 +0300

    Update reports form

--- WPFClient/ReportsForm/FormReportsAll.xaml.cs
+++ WPFClient/ReportsForm/FormReportsAll.xaml.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -12,7 +13,10 @@ using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
 
 using System.Text.RegularExpressions;
+using System.Threading.Tasks;
 using Reports.Public;
+using Data.Entity;
+using Data.Repository;
 
 namespace WPFClient.ReportsForm
 {
@@ -22,15 +26,17 @@ namespace WPFClient.ReportsForm
     public partial class FormReportsAll : Window
     {
         private ReportsCreator reportsCreator;
-        List<string> teachList = new List<string>();
+        private readonly IUow _uow;
+        private readonly App _app = Application.Current as App;
 
         public FormReportsAll()
         {
+            if (_app != null) _uow = _app.Uow;
             InitializeComponent();
             reportsCreator = new ReportsCreator();
-            InitCombo();
+            ReportsCreator.CurrentYear = FileRes.CurrentYear;
             InitCombobox();
-            //this.TeacherCombobox.SelectionChanged += new SelectionChangedEventHandler(TeacherCombobox_SelectionChanged);
+            TeacherCombobox.ItemsSource = _uow.TeacherInfo.All.Select(teacher => teacher.LastName + " " + teacher.Initials).ToList();
         }
 
         private void InitCombobox()
@@ -44,11 +50,6 @@ namespace WPFClient.ReportsForm
             EduTypeK5Combobox.Items.Add("Контракт");
             EduTypeK5Combobox.SelectedItem = EduTypeK5Combobox.Items[0];
 
-            TermOtherCombobox.Items.Add("I семестр");
-            TermOtherCombobox.Items.Add("II семестр");
-            TermOtherCombobox.Items.Add("Всі");
-            TermOtherCombobox.SelectedItem = TermOtherCombobox.Items[0];
-
             DisciplineCombobox.Items.Add("Лекції");
             DisciplineCombobox.Items.Add("Практичні заняття");
             DisciplineCombobox.Items.Add("Лабораторні заняття");
@@ -59,28 +60,6 @@ namespace WPFClient.ReportsForm
 
         }     
 
-        private void InitCombo()
-        {
-            teachList.Add("Рудь");
-            teachList.Add("Храпов");
-            teachList.Add("Чхаїдзе");
-            teachList.Add("Павлов");
-            teachList.Add("Сперкач");
-            teachList.Add("Капітоненко");
-            teachList.Add("Лещенко");
-            teachList.Add("Ромашкін");
-            teachList.Add("Рокосовський");
-            teachList.Add("Самойлов");
-            teachList.Add("Дзюба");
-            teachList.Add("Плющенко");
-            teachList.Add("Морозова");
-            teachList.Add("Дереновський");
-            teachList.Add("Деркач");
-            teachList.Add("Чмутов");
-            teachList.Add("Литвиновський");
-            TeacherCombobox.ItemsSource = teachList;
-        }
-
         private void CloseButton_Click(object sender, RoutedEventArgs e)
         {
             this.Close();
@@ -90,37 +69,67 @@ namespace WPFClient.ReportsForm
         {
             if (K2.IsChecked == true)
             {
-                reportsCreator.CreateK2Async();
+                if (TeacherCombobox.SelectedItem != null)
+                {
+                    ProgressBar.IsIndeterminate = true;
+                    Task task = reportsCreator.CreateK2Async(TeacherCombobox.SelectedItem.ToString(), null);
+                    task.ContinueWith(t => ProgressBar.IsIndeterminate = false, TaskScheduler.FromCurrentSynchronizationContext());
+                }
+                else
+                {
+                    MessageBox.Show("Виберіть викладача!");
+                }
             }
             else if (TotalResults.IsChecked == true)
             {
-                reportsCreator.CreatePlanAllTotalAsync();
+                ProgressBar.IsIndeterminate = true;
+                Task task = reportsCreator.CreatePlanAllTotalAsync(null);
+                task.ContinueWith(t => ProgressBar.IsIndeterminate = false, TaskScheduler.FromCurrentSynchronizationContext());
             }
             else if (PlanAll.IsChecked == true)
             {
-                reportsCreator.CreatePlanAllAsync();
+                ProgressBar.IsIndeterminate = true;
+                Task task = reportsCreator.CreatePlanAllAsync(null);
+                task.ContinueWith(t => ProgressBar.IsIndeterminate = false, TaskScheduler.FromCurrentSynchronizationContext());
             }
             else if (PaymentSum.IsChecked == true)
             {
-                reportsCreator.CreatePaymentSumAsync();
+                if (isFullCheckBox.IsChecked == true)
+                {
+                    ProgressBar.IsIndeterminate = true;
+                    Task task = reportsCreator.CreatePaymentSumAsync(true, null);
+                    task.ContinueWith(t => ProgressBar.IsIndeterminate = false, TaskScheduler.FromCurrentSynchronizationContext());
+                }
+                else
+                {
+                    ProgressBar.IsIndeterminate = true;
+                    Task task = reportsCreator.CreatePaymentSumAsync(false, null);
+                    task.ContinueWith(t => ProgressBar.IsIndeterminate = false, TaskScheduler.FromCurrentSynchronizationContext());
+                }
             }
-           
+
         }
 
         private void CreateK4Button_Click(object sender, RoutedEventArgs e)
         {
             if (EduTypeK4Combobox.SelectedItem == EduTypeK4Combobox.Items[0])
             {
-                reportsCreator.CreateK4Async(EduType.Budget);
+                ProgressBar.IsIndeterminate = true;
+                Task task = reportsCreator.CreateK4Async(EduType.Budget, null);
+                task.ContinueWith(t => ProgressBar.IsIndeterminate = false, TaskScheduler.FromCurrentSynchronizationContext());
             }
             else if (EduTypeK4Combobox.SelectedItem == EduTypeK4Combobox.Items[1])
             {
-                reportsCreator.CreateK4Async(EduType.Contract);
+                ProgressBar.IsIndeterminate = true;
+                Task task = reportsCreator.CreateK4Async(EduType.Contract, null);
+                task.ContinueWith(t => ProgressBar.IsIndeterminate = false, TaskScheduler.FromCurrentSynchronizationContext());
             }
             else
             {
-                reportsCreator.CreateK4Async(EduType.Budget);
-                reportsCreator.CreateK4Async(EduType.Contract);
+                ProgressBar.IsIndeterminate = true;
+                reportsCreator.CreateK4Async(EduType.Budget, null);
+                Task task = reportsCreator.CreateK4Async(EduType.Contract, null);
+                task.ContinueWith(t => ProgressBar.IsIndeterminate = false, TaskScheduler.FromCurrentSynchronizationContext());
             }
             
         }
@@ -129,27 +138,63 @@ namespace WPFClient.ReportsForm
         {
             if (EduTypeK5Combobox.SelectedItem == EduTypeK5Combobox.Items[0])
             {
-                reportsCreator.CreateK5Async(EduType.Budget);
-            }
-            else if (EduTypeK5Combobox.SelectedItem == EduTypeK5Combobox.Items[1])
-            {
-                reportsCreator.CreateK5Async(EduType.Contract);
+                if (TermK5Combobox.SelectedItem == TermK5Combobox.Items[0])
+                {
+                    ProgressBar.IsIndeterminate = true;
+                    Task task = reportsCreator.CreateK5Async(TermK5Combobox.Items[0].ToString(), EduType.Budget, null);           // Budget I semestr
+                    task.ContinueWith(t => ProgressBar.IsIndeterminate = false, TaskScheduler.FromCurrentSynchronizationContext());
+                }
+                else if (TermK5Combobox.SelectedItem == TermK5Combobox.Items[1])
+                {
+                    ProgressBar.IsIndeterminate = true;
+                    Task task = reportsCreator.CreateK5Async(TermK5Combobox.Items[1].ToString(), EduType.Budget, null);           // Budget II semestr
+                    task.ContinueWith(t => ProgressBar.IsIndeterminate = false, TaskScheduler.FromCurrentSynchronizationContext());
+                }
+                else if (TermK5Combobox.SelectedItem == TermK5Combobox.Items[2])
+                {
+                    ProgressBar.IsIndeterminate = true;
+                    Task task = reportsCreator.CreateK5Async(TermK5Combobox.Items[2].ToString(), EduType.Budget, null);           // Budget All
+                    task.ContinueWith(t => ProgressBar.IsIndeterminate = false, TaskScheduler.FromCurrentSynchronizationContext());
+                }
+                else
+                {
+                    MessageBox.Show("Виберіть семестр!");
+                }
             }
             else
             {
-                reportsCreator.CreateK5Async(EduType.Budget);
-                reportsCreator.CreateK5Async(EduType.Contract);
+                if (TermK5Combobox.SelectedItem == TermK5Combobox.Items[0])
+                {
+                    ProgressBar.IsIndeterminate = true;
+                    Task task = reportsCreator.CreateK5Async(TermK5Combobox.Items[0].ToString(), EduType.Contract, null);           // Contract I semestr
+                    task.ContinueWith(t => ProgressBar.IsIndeterminate = false, TaskScheduler.FromCurrentSynchronizationContext());
+                }
+                else if (TermK5Combobox.SelectedItem == TermK5Combobox.Items[1])
+                {
+                    ProgressBar.IsIndeterminate = true;
+                    Task task = reportsCreator.CreateK5Async(TermK5Combobox.Items[1].ToString(), EduType.Contract, null);           // Contract II semestr
+                    task.ContinueWith(t => ProgressBar.IsIndeterminate = false, TaskScheduler.FromCurrentSynchronizationContext());
+                }
+                else
+                {
+                    MessageBox.Show("Виберіть семестр!");
+                }                
             }
         }
 
         private void CreateOtherButton_Click(object sender, RoutedEventArgs e)
         {
-            reportsCreator.CreateLoadDistributionAsync(DisciplineCombobox.SelectedValue.ToString());
+            ProgressBar.IsIndeterminate = true;
+            Task task = reportsCreator.CreateLoadDistributionAsync(DisciplineCombobox.SelectedValue.ToString(), _uow.TeacherInfo.All.Select(teacher => teacher.LastName + " " + teacher.Initials).ToList());
+            task.ContinueWith(t => ProgressBar.IsIndeterminate = false, TaskScheduler.FromCurrentSynchronizationContext());
         }
 
         private void CreateDisciplineButton_Click(object sender, RoutedEventArgs e)
         {
-            reportsCreator.CreatePlanAllDisciplineAsync();
+            ProgressBar.IsIndeterminate = true;
+            Task task = reportsCreator.CreatePlanAllDisciplineAsync(null);
+            task.ContinueWith(t => ProgressBar.IsIndeterminate = false, TaskScheduler.FromCurrentSynchronizationContext());
+            
         }
 
         private void EduTypeK5Combobox_SelectionChanged(object sender, SelectionChangedEventArgs e)
