71b401f658d8456f3132713f4e955bf83f45919f
Vladyslav Khrapov <vlad.hrapov@gmail.com>
2015-06-08 00:06:00 +0300

    Update reports saver

--- Reports/Saver.cs
+++ Reports/Saver.cs
@@ -5,6 +5,7 @@ using System.Text;
 using System.Threading.Tasks;
 
 using System.Diagnostics;
+using System.IO;
 using MigraDoc;
 using MigraDoc.DocumentObjectModel;
 using PdfSharp.Pdf;
@@ -18,5 +19,43 @@ namespace Reports
         protected string fileName;
         public virtual void SaveDocument(object[] document) { }
         public virtual void SaveDocument(Document document) { }
+
+        public void InitDirectoryStructure()
+        {
+            if(Directory.Exists(Environment.CurrentDirectory + @"\Звіти"))
+            {
+                if (!Directory.Exists(Environment.CurrentDirectory + @"\Звіти\Архів"))
+                {
+                    Directory.CreateDirectory(Environment.CurrentDirectory + @"\Звіти\Архів");
+                    Directory.CreateDirectory(Environment.CurrentDirectory + @"\Звіти\Архів\" + DateTime.Now.Year.ToString());
+                }
+                else
+                {
+                    if (!Directory.Exists(Environment.CurrentDirectory + @"\Звіти\Архів\" + DateTime.Now.Year.ToString()))
+                    {
+                        Directory.CreateDirectory(Environment.CurrentDirectory + @"\Звіти\Архів\" + DateTime.Now.Year.ToString());
+                    }
+                }
+                if (!Directory.Exists(Environment.CurrentDirectory + @"\Звіти\Поточні"))
+                {
+                    Directory.CreateDirectory(Environment.CurrentDirectory + @"\Звіти\Поточні");
+                    Directory.CreateDirectory(Environment.CurrentDirectory + @"\Звіти\Поточні\" + Enum.GetName(typeof(Monthes), DateTime.Now.Month) + " " + DateTime.Now.Year);
+                }
+                if (!Directory.Exists(Environment.CurrentDirectory + @"\Звіти\Поточні\" + Enum.GetName(typeof(Monthes), DateTime.Now.Month) + " " + DateTime.Now.Year))
+                {
+                    Directory.CreateDirectory(Environment.CurrentDirectory + @"\Звіти\Поточні\" + Enum.GetName(typeof(Monthes), DateTime.Now.Month) + " " + DateTime.Now.Year);
+                }
+            }
+            else
+            {
+                Directory.CreateDirectory(Environment.CurrentDirectory + @"\Звіти");
+                Directory.CreateDirectory(Environment.CurrentDirectory + @"\Звіти\Архів");
+                Directory.CreateDirectory(Environment.CurrentDirectory + @"\Звіти\Архів\" + DateTime.Now.Year.ToString());
+                Directory.CreateDirectory(Environment.CurrentDirectory + @"\Звіти\Поточні");
+                Directory.CreateDirectory(Environment.CurrentDirectory + @"\Звіти\Поточні\" + Enum.GetName(typeof(Monthes), DateTime.Now.Month) + " " + DateTime.Now.Year);
+            }
+        }
+
+
     }
 }
